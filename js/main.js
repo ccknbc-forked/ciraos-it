b9eb370ddbb1597cd5586baf2805e7a92d9a75da
ciraos <frank.ka@foxmail.com>
2022-08-11 23:24:01 +0800

    F 更改主页header背景/20220809

--- js/main.js
+++ js/main.js
@@ -2,3 +2,14 @@
 /**
  * main.js
  */
+
+/*
+ * scroll-down
+ */
+
+/* $('#scrolldown').click(
+    function () {
+        $('html,body').animate({
+            scrollTop: document.getElementById('content')[0].scrollHeight
+        }, 2000); return false;
+    }); */
